5c6ba16569aea8868dae1c4b36e17594bed43e8b
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2023-02-26 22:58:44 +0300

    -

---
+++ HAKERRANK0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+///C# kodu Görevi Bu zorluğu tamamlamak için, stdin'den bir değişkene, print Hello, World'e bir girdi satırı kaydetmeniz gerekir. tek bir satırda ve son olarak değişkeninizin değerini ikinci bir satırda yazdırın.
+
+namespace HAKERRANK
+{
+    using System;
+
+class Program4 {
+    static void Main(string[] args) {
+        // Sabit Değer
+        Console.WriteLine("Hello, World.");
+
+         // Bizim belirlediğimiz değişken kısım
+        string degisken = Console.ReadLine();
+
+        // Belirlediğimiz değişken çıktı
+        Console.WriteLine(degisken);
+    }
+}
+}
